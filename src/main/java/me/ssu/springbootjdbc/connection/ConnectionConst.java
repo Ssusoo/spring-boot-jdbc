08abe283d04161a5c5d9f14793e4580b24eb3613
김수환 <zzanggoon8@hiconsy.com>
2022-11-02 17:07:23 +0900

    Connection basic info abstract class Add

---
+++ src/main/java/me/ssu/springbootjdbc/connection/ConnectionConst.java
@@ -0,0 +1,8 @@
+package me.ssu.springbootjdbc.connection;
+
+public abstract class ConnectionConst {
+	// TODO 1) 기본정보를 편리하게 사용할 수 있게 상수로 만들기
+	public static final String URL = "jdbc:h2:tcp://localhost/~/test";
+	public static final String USERNAME = "sa";
+	public static final String PASSWORD = "";
+}
