b1e3e09b2c5dce4cd667ef610ee10aef603dd641
김수환 <zzanggoon8@hiconsy.com>
2022-11-02 18:08:49 +0900

    TDD, Member type save data check

--- src/test/java/me/ssu/springbootjdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/me/ssu/springbootjdbc/repository/MemberRepositoryV0Test.java
@@ -1,6 +1,7 @@
 package me.ssu.springbootjdbc.repository;
 
 import me.ssu.springbootjdbc.domain.Member;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
@@ -12,6 +13,7 @@ class MemberRepositoryV0Test {
 	MemberRepositoryV0 repository = new MemberRepositoryV0();
 
 	@Test
+	@DisplayName("회원 데이터 저장")
 	void crud() throws SQLException {
 		Member member = new Member("memberV0", 10000);
 		repository.save(member);
