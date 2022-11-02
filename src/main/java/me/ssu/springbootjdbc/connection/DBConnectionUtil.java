03095d652a94a404fd5b26c5ad006e21fb3e7faa
김수환 <zzanggoon8@hiconsy.com>
2022-11-02 17:15:51 +0900

    Connection Util class Add

--- src/main/java/me/ssu/springbootjdbc/connection/DBConnectionUtil.java
+++ src/main/java/me/ssu/springbootjdbc/connection/DBConnectionUtil.java
@@ -13,9 +13,12 @@ public class DBConnectionUtil {
 	// TODO 1) 자바에서 제공하는 ConnectionPool
 	public static Connection getConnection() {
 		try {
-			// TODO 2)  JDBC가 제공하는 드라이버
+			// TODO 2)  JDBC가 제공하는 드라이버(데이터베이스를 연결하기 위해)
 			Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-			log.info("get connection={}, class={}",connection, connection.getClass());
+			log.info("get connection={}, class={}",
+					connection,
+					connection.getClass()   // 클래스 정보(connection.getClass)
+			);
 			return connection;
 		} catch (SQLException e) {
 			throw new IllegalStateException(e);
